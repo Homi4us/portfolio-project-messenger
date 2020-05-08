74e08b5d0817367f1bd90ec010e3fd54137769a3
GentleGame <banderes94@gmail.com>
2020-05-08 22:54:04 +0300

    before merge

--- games/durak/game.js
+++ games/durak/game.js
@@ -1,6 +1,11 @@
 class Game{
     constructor(){
-        
+        console.log("Durak successfully run")
+    }
+    rooms = []
+    createRoom(socket){
+        socket.join(rooms.length.toString())
+        this.rooms.push({players: [socket.nickname]})
     }
 }
 module.exports = Game
---
+++ games/durak/main.js
@@ -0,0 +1,4 @@
+const Game = require('./game')
+
+const Game = new Game()
+
