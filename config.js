f0a990376bdf6fec2c05dd05dff2a84d84113ba0
Homi4us <62098299+Homi4us@users.noreply.github.com>
2023-02-09 02:36:23 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://Alex:19121402@messenger-shard-00-00-naqfx.mongodb.net:27017,messenger-shard-00-01-naqfx.mongodb.net:27017,messenger-shard-00-02-naqfx.mongodb.net:27017/test?ssl=true&replicaSet=messenger-shard-0&authSource=admin&retryWrites=true&w=majority',
+    'mongoUrl' : '',
     'serverUrl': 'http://localhost:3001'
 }
